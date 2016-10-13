e0f93a32eaa74aac0eac50226c7551ab033e9dc7
배근호 <fanta52b@naver.com>
2016-10-13 13:25:10 +0900

    no message

--- final_project/src/main/java/com/jhta/finalproject/SearchController.java
+++ final_project/src/main/java/com/jhta/finalproject/SearchController.java
@@ -21,6 +21,7 @@ public class SearchController {
 	@RequestMapping("/search/track")//검색어 입력시 가장 처음 실행되는 controller(즉, track 검색이 기본값)
 	public String search(Model model,String keyword,int pageNum) {
 		HashMap<String, String> searchMap = new HashMap<String, String>();
+		searchMap.put("pageNum", pageNum);
 		
 		List<Track_Dto> result = service.searchTrack(searchMap);
 		return ".search";
